d32d2583ea80e95f841a153319e0081077bde412
luis1gonzalez <luis1gonzalez@hotmail.com>
2023-03-04 16:20:35 +0100

    corrigiendo error con cors

--- src/models/importsTools.js
+++ src/models/importsTools.js
@@ -1,6 +1,9 @@
 
     export async function getTools(){
-      const response = await fetch(import.meta.env.VITE_BACKEND_URL_TOOLS)
+      const response = await fetch(import.meta.env.VITE_BACKEND_URL_TOOLS, {
+        mode: 'cors',
+        credentials: 'include'
+      })
       return await response.json()
       
     }
