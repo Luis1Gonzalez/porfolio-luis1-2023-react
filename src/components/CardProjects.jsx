ada5c38e36c49fe7b7e8729972e4afabf4ef3bc7
luis1gonzalez <luis1gonzalez@hotmail.com>
2023-03-04 14:55:41 +0100

    corrigiendo error en el fetch

--- src/components/CardProjects.jsx
+++ src/components/CardProjects.jsx
@@ -3,40 +3,37 @@ import { useEffect, useState } from "react"
 // import { getCards } from "../models/cards.server"
 
 export default function CardProjects() {
-    const [importCards, setImportCards] = useState({})
+  const [importCards, setImportCards] = useState({})
 
-    useEffect(() => {
-        const getCards = async() => {
-const response = await fetch(import.meta.env.VITE_BACKEND_URL_CARDS)
+  useEffect(() => {
+    const getCards = async () => {
+      const response = await fetch(import.meta.env.VITE_BACKEND_URL_CARDS)
 
-const result = await response.json()
+      const result = await response.json()
 
-setImportCards(result)
-        }
-        // fetch(import.meta.env.VITE_BACKEND_URL_CARDS)
+      setImportCards(result)
+    }
 
-        // .then((response => response.json()))
-        // .then(c => setImportCards(c))
-        getCards()
-    },[importCards])
+    getCards()
+  }, [])
 
-    return (
-        <div className='bg-zinc-900 p-6 w-100'>
-        
-          <h1 className='text-white font-black text-2xl' >Services</h1>
-    
-          <div className="text-stone-200 md:flex flex-wrap justify-around sm:flex">
-            {importCards?.data?.map(card => (
-    
-              <div key={card.id} className="my-4 shadow-lg shadow-stone-200/20 sm:w-[47%] md:mx-4 xl:w-[30%] py-2">
-               
-                  <div className=''>
-                    <img src={card.attributes.image.data.attributes.url} className='' alt={`Foto de la card ${card.name}`} />
-                  </div>                  
-                </div>     
-            ))}
+  return (
+    <div className='bg-zinc-900 p-6 w-100'>
+
+      <h1 className='text-white font-black text-2xl' >Services</h1>
+
+      <div className="text-stone-200 md:flex flex-wrap justify-around sm:flex">
+        {importCards?.data?.map(card => (
+
+          <div key={card.id} className="my-4 shadow-lg shadow-stone-200/20 sm:w-[47%] md:mx-4 xl:w-[30%] py-2">
+
+            <div className=''>
+              <img src={card.attributes.image.data.attributes.url} className='' alt={`Foto de la card ${card.name}`} />
+            </div>
           </div>
-    
-        </div>
-      )
+        ))}
+      </div>
+
+    </div>
+  )
 }
