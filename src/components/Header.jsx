df1185caa93b07c0384f1f6d1c69130578cab9cc
luis1gonzalez <luis1gonzalez@hotmail.com>
2023-03-04 17:45:33 +0100

    corrigiendo error con cors

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -13,7 +13,7 @@ export default function Header() {
             </div>
             
 
-            <div className='bg-inherit w-[50%] h-[100%] text-stone-200 flex flex-col sm:tracking-[.20em] lg:py-24 pl-9 text-sm  sm:text-2xl md:text-3xl lg:text-4xl lg:font-black justify-center lg:justify-around'>
+            <div className='bg-inherit w-[60%] h-[100%] text-stone-200 flex flex-col sm:tracking-[.20em] lg:py-24 pl-9   sm:text-2xl md:text-3xl lg:text-4xl lg:font-black justify-center lg:justify-around'>
                 <p className='text-emerald-300'>Hello</p>
                 <p className=' font-bold'>I'm Luis</p>
                 <p className='text-emerald-300'>Frontend Developer</p>
--- src/components/Tools.jsx
+++ src/components/Tools.jsx
@@ -1,31 +1,17 @@
 import React, { useEffect, useState } from 'react'
-import { getTools } from '../models/importsTools'
 
 export default function Tools() {
 const [tools, setTools] = useState({})
 
-// useEffect(() => {
-//   const getTools = async () => {
-//     const response = await fetch(import.meta.env.VITE_BACKEND_URL_TOOLS)
-//     const result = await response.json()
-//     setTools(result)
-//   }
-//   getTools()
-// }, [])
-
-  async function cargador(){
-    const esto = await getTools()
-    setTools(esto)
-  }
 useEffect(() => {
-  cargador()
-},[])
+  const getTools = async () => {
+    const response = await fetch(import.meta.env.VITE_BACKEND_URL_TOOLS)
+    const result = await response.json()
+    setTools(result)
+  }
+  getTools()
+}, [])
 
-
-
-
-
-console.log('caramba')
   return (
     <div className='p-4 w-100 mb-5'>
     
@@ -34,7 +20,7 @@ console.log('caramba')
       <div className="text-stone-200 flex flex-wrap justify-around sm:px-10">
         {tools?.data?.map(tool => (
 
-          <div key={tool.id} className="my-2 w-[20%] sm:w-[10%] mx-3 flex items-center justify-center">
+          <div key={tool.id} className="my-2 w-[12%] sm:w-[10%] mx-3 flex items-center justify-center">
            
               <div className=''>
                 <img src={tool.attributes.image.data[0].attributes.url} className='' alt={`Foto de las herramientas ${tool.name}`} />
